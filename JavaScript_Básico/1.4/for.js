f73d0b30bfdb34254a49e2e289755fc01878b1ef
Joao Pedro Rodrigues <jp.rgs.2015@gmail.com>
2025-04-24 18:19:00 -0300

    Operador - for

---
+++ JavaScript_Básico/1.4/for.js
@@ -0,0 +1,9 @@
+for (let i = 0; i<=5 ; i++ ) {
+    console.log(`linha ${i}`)
+}
+
+const frutas = ['maça','pera','uva']
+for (i=0; i < frutas.length; i++){
+    console.log(`Indice ${i} ${frutas[i]}`)
+}
+
